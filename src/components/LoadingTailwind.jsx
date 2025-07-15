300216905dc03a6a9e8cc699947786eeee88147e
idsantri <idsantri.mail@gmail.com>
2025-07-15 18:12:59 +0700

    loading tailwind

--- src/components/LoadingTailwind.jsx
+++ src/components/LoadingTailwind.jsx
@@ -1,20 +1,18 @@
 const LoadingTailwind = ({ children = '' }) => {
 	return (
-		<>
-			<div
-				data-testid="loading"
-				className="absolute top-0 left-0 bottom-0 right-0 flex items-center justify-center flex-col"
-			>
-				<div className="flex items-center justify-center gap-x-2 ">
-					<div className="w-8 h-8  animate-pulse bg-[#b1d991] rounded-full"></div>
-					<div className="w-8 h-8 animate-pulse  bg-[#4fb85e] rounded-full"></div>
-					<div className="w-8 h-8 animate-pulse bg-[#2a6424] rounded-full"></div>
-				</div>
-				<div className="p-2">
-					{children || 'Tunggu sebentar, sedang memuat data …'}
-				</div>
+		<div
+			data-testid="loading"
+			className="fixed top-0 left-0 bottom-0 right-0 flex items-center justify-center flex-col bg-black/25 z-1000"
+		>
+			<div className="flex items-center justify-center gap-x-2">
+				<div className="w-8 h-8 animate-pulse bg-[#b1d991] rounded-full"></div>
+				<div className="w-8 h-8 animate-pulse bg-[#4fb85e] rounded-full"></div>
+				<div className="w-8 h-8 animate-pulse bg-[#2a6424] rounded-full"></div>
 			</div>
-		</>
+			<div className="p-2 text-white">
+				{children || 'Tunggu sebentar, sedang memuat data …'}
+			</div>
+		</div>
 	);
 };
 
--- src/pages/sales/SaleDetail.jsx
+++ src/pages/sales/SaleDetail.jsx
@@ -1,7 +1,9 @@
-import { useParams } from 'react-router';
+import { Link, useParams } from 'react-router';
 import sales from '../../models/sales';
 import { useEffect, useState } from 'react';
 import LoadingTailwind from '../../components/LoadingTailwind';
+import formatDate from '../../utils/format-date';
+import { Icon } from '@iconify/react/dist/iconify.js';
 
 const SaleDetail = () => {
 	const { id } = useParams();
@@ -23,15 +25,56 @@ const SaleDetail = () => {
 	}, [id]);
 
 	return (
-		<div className="">
+		<div className="pointer-events-auto">
 			<div className="text-center">
 				<h2 className="text-xl font-bold">Detail Penjualan</h2>
 				{isLoading ? (
 					<LoadingTailwind />
 				) : (
-					<div className="mt-4">
-						OK
-						{/* <pre>{JSON.stringify(saleData, null, 2)}</pre> */}
+					<div className="mt-4 card card-border rounded-md">
+						{/* <LoadingTailwind /> */}
+						<div className="overflow-x-auto">
+							<table className="table">
+								<tbody>
+									<tr>
+										<td>Tanggal</td>
+										<td>
+											{formatDate(
+												data.sale_date,
+												'dd MMMM yyyy',
+											)}
+										</td>
+									</tr>
+									<tr>
+										<td>Pelanggan</td>
+										<td className="flex items-center justify-between">
+											{data.customer.name}
+											<Link
+												className="btn btn-xs btn-info btn-circle text-info-content border-primary"
+												to={`/customers/${data.customer.id}`}
+											>
+												<Icon
+													icon="material-symbols:info"
+													width="1.5em"
+												/>
+											</Link>
+										</td>
+									</tr>
+									<tr>
+										<td>Status</td>
+										<td>{data.status}</td>
+									</tr>
+									<tr>
+										<td>Terbayar</td>
+										<td>{data.total_amount}</td>
+									</tr>
+									<tr>
+										<td>Catatan</td>
+										<td>{data.note || '-'}</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
 					</div>
 				)}
 			</div>
