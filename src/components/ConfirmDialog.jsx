2918fb0bfbde8229b24520cc0b42309dedfd6d25
idsantri <idsantri.mail@gmail.com>
2025-07-16 09:39:51 +0700

    dialog confirmation

--- src/components/ConfirmDialog.jsx
+++ src/components/ConfirmDialog.jsx
@@ -1,21 +1,36 @@
-const ConfirmDialog = ({ title, message, onConfirm, onCancel }) => (
-	<div className="fixed inset-0 z-50 flex items-center justify-center bg-blue-900 bg-opacity-70">
-		<div className="px-6 py-4 rounded-md shadow-lg bg-blue-300 w-80">
-			<h2 className="mb-2 text-lg font-medium text-blue-800">{title}</h2>
-			<p className="mb-4 text-sm font-light text-blue-900">{message}</p>
-			<div className="flex justify-end space-x-2">
-				<button
-					className="w-16 font-medium border-none rounded-md shadow-sm btn btn-outline btn-sm bg-blue-200 text-blue-700 shadow-blue-600"
-					onClick={onCancel}
-				>
-					Tidak
-				</button>
-				<button
-					className="w-16 font-medium border-none rounded-md shadow-sm btn btn-sm bg-blue-700 text-blue-200 shadow-blue-100 hover:text-blue-950"
-					onClick={onConfirm}
-				>
-					Ya
-				</button>
+const ConfirmDialog = ({
+	title = 'Konfirmasi!',
+	message,
+	onConfirm,
+	onCancel,
+}) => (
+	<div className="fixed inset-0 z-1000 flex items-center justify-center bg-primary-content/80">
+		<div className="rounded-md shadow-lg bg-info w-80 overflow-hidden">
+			<div className="px-2 py-1 bg-warning text-warning-content">
+				<h3 className="text-lg font-medium">{title}</h3>
+			</div>
+			<div className="px-2 py-2 bg-warning/75 border-t border-warning-content/10">
+				<p className="text-sm font-light text-warning-content">
+					{message}
+				</p>
+				<div className="flex justify-end space-x-3 mt-2">
+					<button
+						className="w-16 font-medium border-none rounded-md shadow-sm btn btn-outline btn-sm 
+						bg-warning text-warning-content shadow-warning-content 
+						hover:bg-info hover:text-info-content hover:shadow-info-content"
+						onClick={onCancel}
+					>
+						Tidak
+					</button>
+					<button
+						className="w-16 font-medium border-none rounded-md shadow-sm btn btn-outline btn-sm 
+						bg-neutral text-neutral-content shadow-neutral-content 
+						hover:bg-success hover:text-success-content hover:shadow-success-content"
+						onClick={onConfirm}
+					>
+						Ya
+					</button>
+				</div>
 			</div>
 		</div>
 	</div>
