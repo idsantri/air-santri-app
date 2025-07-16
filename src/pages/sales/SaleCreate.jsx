21049b3fd9ce2372173b207a28be5900309835fd
idsantri <idsantri.mail@gmail.com>
2025-07-16 19:01:00 +0700

    form ongoing

--- src/pages/sales/SaleCreate.jsx
+++ src/pages/sales/SaleCreate.jsx
@@ -1,8 +1,58 @@
 const SaleCreate = () => {
+	const onSubmit = (e) => {
+		e.preventDefault();
+		console.log(e);
+	};
 	return (
-		<div>
-			<h2>Sale Create</h2>
-		</div>
+		<>
+			<div className="card p-2 border border-base-200 rounded-sm">
+				<header>
+					<div className="flex items-center justify-between  rounded-sm p-2 bg-base-200 mb-2">
+						<h2 className="text-xl text-neutral">
+							Formulir Transaksi
+						</h2>
+						<p className="badge badge-info">Baru</p>
+					</div>
+				</header>
+
+				<form onSubmit={onSubmit} className="flex flex-col gap-4 mt-6">
+					<label className="floating-label">
+						<span>Pelanggan (ID)</span>
+						<input type="text" className="input w-full" />
+					</label>
+					<label className="floating-label">
+						<span>Agen (ID)</span>
+						<input
+							type="text"
+							className="input w-full"
+							disabled
+							value={'123'}
+						/>
+					</label>
+					<label className="floating-label">
+						<span>Tanggal</span>
+						<input type="text" className="input w-full" />
+					</label>
+					<label className="floating-label">
+						<span>Status</span>
+						<input type="text" className="input w-full" />
+					</label>
+					<label className="floating-label">
+						<span>Catatan</span>
+						<textarea type="text" className="textarea w-full" />
+					</label>
+					<div className="flex items-center justify-between ">
+						<button className="btn btn-warning">Hapus</button>
+						<div className="flex items-center gap-2">
+							<button className="btn btn-secondary">Gagal</button>
+							<button type="submit" className="btn btn-primary">
+								Simpan
+							</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</>
 	);
 };
 export default SaleCreate;
--- src/pages/sales/SaleDetail.jsx
+++ src/pages/sales/SaleDetail.jsx
@@ -30,120 +30,116 @@ const SaleDetail = () => {
 	}, [id]);
 
 	return (
-		<div className="pointer-events-auto">
-			<div className="">
-				<div className="flex items-center justify-between  rounded-sm p-2 bg-base-200 mb-2">
-					<h2 className="text-xl text-neutral">Detail Penjualan</h2>
-					<Link
-						className="btn btn-sm btn-neutral rounded-sm"
-						to={`/sales/${id}/edit`}
-					>
-						<Icon icon="material-symbols:edit-rounded" />
-						Edit
-					</Link>
-				</div>
-				{isLoading ? (
-					<LoadingTailwind />
-				) : (
-					<>
-						<div className="card card-border rounded-sm">
-							<div className="overflow-x-auto">
-								<table className="table">
-									<tbody>
-										<tr>
-											<td>Tanggal</td>
-											<td>
-												{formatDate(
-													data.sale_date,
-													'dd MMMM yyyy',
-												)}
-											</td>
-										</tr>
-										<tr>
-											<td>Invoice</td>
-											<td>{data.code}</td>
-										</tr>
-										<tr>
-											<td>Pelanggan</td>
-											<td className="flex items-center justify-between">
-												{data.customer_name}
-												<Link
-													className="btn btn-sm btn-info btn-circle text-neutral border-neutral-400 absolute right-2"
-													to={`/customers/${data.customer_id}`}
-												>
-													<Icon
-														icon="material-symbols:info"
-														width="1.5em"
-													/>
-												</Link>
-											</td>
-										</tr>
-										<tr>
-											<td>Status</td>
-											<td>{data.status}</td>
-										</tr>
-										<tr>
-											<td>Tagihan</td>
-											<td>
-												{data?.total_amount &&
-													data.total_amount?.toRupiah()}{' '}
-												<span className="text-xs italic">
-													({details?.length || 0}{' '}
-													item)
-												</span>
-											</td>
-										</tr>
-										<tr>
-											<td>Terbayar</td>
-											<td>
-												{data?.total_payment &&
-													data.total_payment.toRupiah()}{' '}
-												<span className="text-xs italic">
-													({payments?.length || 0}{' '}
-													kali)
-												</span>
-											</td>
-										</tr>
-
-										<tr>
-											<td>Sisa</td>
-											<td>
-												{(
-													data.total_amount -
-													data.total_payment
-												).toRupiah()}
-											</td>
-										</tr>
-
-										<tr>
-											<td>Agen</td>
-											<td className="flex items-center justify-between">
-												{data.warehouse_name}
-												<Link
-													className="btn btn-sm btn-info btn-circle text-neutral border-neutral-400 absolute right-2"
-													to={`/warehouses/${data.warehouse_id}`}
-												>
-													<Icon
-														icon="material-symbols:info"
-														width="1.5em"
-													/>
-												</Link>
-											</td>
-										</tr>
-
-										<tr>
-											<td>Catatan</td>
-											<td>{data.note || '-'}</td>
-										</tr>
-									</tbody>
-								</table>
-							</div>
-						</div>
-						<SaleDetailTab details={details} payments={payments} />
-					</>
-				)}
+		<>
+			<div className="flex items-center justify-between  rounded-sm p-2 bg-base-200 mb-2">
+				<h2 className="text-xl text-neutral">Detail Penjualan</h2>
+				<Link
+					className="btn btn-sm btn-neutral rounded-sm"
+					to={`/sales/${id}/edit`}
+				>
+					<Icon icon="material-symbols:edit-rounded" />
+					Edit
+				</Link>
 			</div>
-		</div>
+			{isLoading ? (
+				<LoadingTailwind />
+			) : (
+				<>
+					<div className="card card-border rounded-sm">
+						<div className="overflow-x-auto">
+							<table className="table">
+								<tbody>
+									<tr>
+										<td>Tanggal</td>
+										<td>
+											{formatDate(
+												data.sale_date,
+												'dd MMMM yyyy',
+											)}
+										</td>
+									</tr>
+									<tr>
+										<td>Invoice</td>
+										<td>{data.code}</td>
+									</tr>
+									<tr>
+										<td>Pelanggan</td>
+										<td className="flex items-center justify-between">
+											{data.customer_name}
+											<Link
+												className="btn btn-sm btn-info btn-circle text-neutral border-neutral-400 absolute right-2"
+												to={`/customers/${data.customer_id}`}
+											>
+												<Icon
+													icon="material-symbols:info"
+													width="1.5em"
+												/>
+											</Link>
+										</td>
+									</tr>
+									<tr>
+										<td>Status</td>
+										<td>{data.status}</td>
+									</tr>
+									<tr>
+										<td>Tagihan</td>
+										<td>
+											{data?.total_amount &&
+												data.total_amount?.toRupiah()}{' '}
+											<span className="text-xs italic">
+												({details?.length || 0} item)
+											</span>
+										</td>
+									</tr>
+									<tr>
+										<td>Terbayar</td>
+										<td>
+											{data?.total_payment &&
+												data.total_payment.toRupiah()}{' '}
+											<span className="text-xs italic">
+												({payments?.length || 0} kali)
+											</span>
+										</td>
+									</tr>
+
+									<tr>
+										<td>Sisa</td>
+										<td>
+											{(
+												data.total_amount -
+												data.total_payment
+											).toRupiah()}
+										</td>
+									</tr>
+
+									<tr>
+										<td>Agen</td>
+										<td className="flex items-center justify-between">
+											{data.warehouse_name}
+											<Link
+												className="btn btn-sm btn-info btn-circle text-neutral border-neutral-400 absolute right-2"
+												to={`/warehouses/${data.warehouse_id}`}
+											>
+												<Icon
+													icon="material-symbols:info"
+													width="1.5em"
+												/>
+											</Link>
+										</td>
+									</tr>
+
+									<tr>
+										<td>Catatan</td>
+										<td>{data.note || '-'}</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+					<SaleDetailTab details={details} payments={payments} />
+				</>
+			)}
+		</>
 	);
 };
 export default SaleDetail;
